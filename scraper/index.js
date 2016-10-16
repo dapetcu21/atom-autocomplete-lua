49570f51f82326d47d955050b914e5b8c1df7bcf
Marius Petcu <dapetcu21@gmail.com>
2016-10-16 23:45:41 +0300

    Implement named types. Finish work on scraper

--- scraper/index.js
+++ scraper/index.js
@@ -5,8 +5,13 @@ const jsdom = require('jsdom')
 const includes = require('lodash.includes')
 const fs = require('fs')
 const path = require('path')
-const luaVersion = '5.2'
+const luaVersion = process.argv[2]
 const blacklist = ['basic', '_G']
+
+if (!luaVersion) {
+  console.log('usage: node scraper <luaVersion>')
+  return
+}
 
 const pagesToCrawl = {}
 const symbols = []
@@ -44,7 +49,10 @@ function onEnd () {
 }
 
 function processSymbols () {
-  const options = { global: { type: 'table', fields: {} } }
+  const options = {
+    global: { type: 'table', fields: {} },
+    namedTypes: {}
+  }
   const _setField = (table, components, value) => {
     if (components.length === 1) {
       table.fields[components[0]] = value
@@ -62,10 +70,28 @@ function processSymbols () {
   }
   const setField = (name, value) => _setField(options.global, name.split('.'), value)
 
+  const setFieldInNamedType = (base, name, value) => {
+    let type = options.namedTypes[base]
+    if (!type) {
+      type = options.namedTypes[base] = {
+        type: 'table',
+        fields: {},
+        metatable: {
+          type: 'table',
+          fields: { __index: { type: 'table', fields: {} } }
+        }
+      }
+    }
+    type.metatable.fields.__index.fields[name] = value
+  }
+
   symbols.forEach(symbol => {
+    const nameSplit = symbol.name.split(':')
+    const hasSelf = nameSplit.length > 1
+
     const match = symbol.title.match(/\((.*)\)$/)
     let type = 'unknown'
-    let args, argsDisplay
+    let args, argsDisplay, argsDisplayOmitSelf
 
     if (match) {
       type = 'function'
@@ -76,19 +102,42 @@ function processSymbols () {
         .split(',')
         .map(s => ({ name: s.replace(/^\s*|\s*$/g, '') }))
         .filter(arg => arg.name.replace(/[^a-zA-Z0-9]/g, '').length)
+
+      if (hasSelf) {
+        argsDisplayOmitSelf = argsDisplay
+        argsDisplay = argsDisplay ? ('self, ' + argsDisplay) : 'self'
+        args.splice(0, 0, { name: 'self' })
+      }
     }
 
-    setField(symbol.name, {
+    const typeDef = {
       link: symbol.link,
       description: symbol.description,
       type,
       args,
-      argsDisplay
-    })
+      argsDisplay,
+      argsDisplayOmitSelf
+    }
+
+    if (hasSelf) {
+      setFieldInNamedType(nameSplit[0], nameSplit[1], typeDef)
+    } else {
+      setField(symbol.name, typeDef)
+    }
   })
 
+  const io = options.global.fields.io;
+  (['open', 'popen', 'tmpfile']).forEach(fname => {
+    io.fields[fname].returnTypes = [{ type: 'ref', name: 'file' }]
+  });
+  (['stdin', 'stdout', 'stderr']).forEach(varname => {
+    io.fields[varname] = { type: 'ref', name: 'file' }
+  })
+  options.global.fields._VERSION.type = 'string'
+  options.global.fields.math.fields.pi.type = 'number'
+
   const filePath = path.join(__dirname, '..', 'lib', 'stdlib', `${luaVersion.replace(/\./g, '_')}.json`)
-  fs.writeFile(filePath, JSON.stringify(options))
+  fs.writeFile(filePath, JSON.stringify(options, null, 2))
 }
 
 let parsingFunctions = false
