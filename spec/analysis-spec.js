8252b82b9dcedf4fe6f819d43f8a88b36e1792f2
Marius Petcu <dapetcu21@gmail.com>
2017-02-21 01:09:11 +0200

    Write more tests and fix formatter

--- spec/analysis-spec.js
+++ spec/analysis-spec.js
@@ -4,11 +4,14 @@
 import Analysis from '../lib/analysis'
 import { tableNew, contextNew } from '../lib/typedefs'
 
+const getNames = (suggestions) => suggestions.map(suggestion => suggestion.text)
+
 async function getSuggestions (options, text, prefix = '', dot = null) {
   const analysis = new Analysis(options, { prefix, dot })
   analysis.end(text)
-  const results = await analysis.solveQuery()
-  return results.map(suggestion => suggestion.text)
+  let results = await analysis.solveQuery()
+  results.sort((a, b) => a.text.localeCompare(b.text))
+  return results
 }
 
 describe('When analysing with empty options', () => {
@@ -26,7 +29,67 @@ describe('When analysing with empty options', () => {
       const suggestions = await getSuggestions(options, `
         __prefix_placeholder__.__prefix_placeholder__()
       `)
-      expect(suggestions).toEqual(['_G'])
+      expect(getNames(suggestions)).toEqual(['_G'])
+      expect(suggestions[0].rightLabel).toEqual('table')
+    })
+  })
+
+  it('should suggest local variable', () => {
+    waitsForPromise(async () => {
+      const suggestions = await getSuggestions(options, `
+        local a = 42
+        __prefix_placeholder__.__prefix_placeholder__()
+      `)
+      expect(getNames(suggestions)).toEqual(['_G', 'a'])
+      expect(suggestions[1].rightLabel).toEqual('number')
+    })
+  })
+
+  it('should suggest global variable', () => {
+    waitsForPromise(async () => {
+      const suggestions = await getSuggestions(options, `
+        a = 42
+        __prefix_placeholder__.__prefix_placeholder__()
+      `)
+      expect(getNames(suggestions)).toEqual(['_G', 'a'])
+      expect(suggestions[1].rightLabel).toEqual('number')
+    })
+  })
+
+  it('should suggest table members', () => {
+    waitsForPromise(async () => {
+      const suggestions = await getSuggestions(options, `
+        a = { foo = 42 }
+        a.bar = 'baz'
+        a.__prefix_placeholder__()
+      `, '', '.')
+      expect(getNames(suggestions)).toEqual(['bar', 'foo'])
+      expect(suggestions[0].rightLabel).toEqual('string')
+      expect(suggestions[1].rightLabel).toEqual('number')
+    })
+  })
+
+  it('should suggest merging of double assignment', () => {
+    waitsForPromise(async () => {
+      const suggestions = await getSuggestions(options, `
+        a = { foo = 42 }
+        a = { bar = 'baz' }
+        a.__prefix_placeholder__()
+      `, '', '.')
+      expect(getNames(suggestions)).toEqual(['bar', 'foo'])
+      expect(suggestions[0].rightLabel).toEqual('string')
+      expect(suggestions[1].rightLabel).toEqual('number')
+    })
+  })
+
+  it('should suggest function', () => {
+    waitsForPromise(async () => {
+      const suggestions = await getSuggestions(options, `
+        function foo() end
+        __prefix_placeholder__.__prefix_placeholder__()
+      `)
+      expect(getNames(suggestions)).toEqual(['_G', 'foo'])
+      expect(suggestions[1].rightLabel).toEqual('function')
     })
   })
 })
